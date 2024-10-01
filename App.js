5c3d3a40513a2fc6a603896e47eb3de0b862ebb5
Akbar Al Farisi Rambe <spaceagain69@gmail.com>
2024-10-01 23:17:55 +0700

    App login dan register

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+
+  function App() {
+    return (
+      <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+    );
+  }
+export default App;
